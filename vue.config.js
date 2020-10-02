e3a4ccd68a6242b7b360e6d9bdb41743ce7ba687
Rex_surprise <@example.com>
2020-10-02 23:14:01 +0800

    'resubmit'

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {  //配置跨域
       '/api': {
-        target: 'http://192.168.0.125:5000',  //这里后台的地址模拟的;应该填写你们真实的后台接口
+        target: 'http://75yg62.natappfree.cc/',  //这里后台的地址模拟的;应该填写你们真实的后台接口
         changOrigin: true,  //允许跨域
         pathRewrite: {
           /* 重写路径，当我们在浏览器中看到请求的地址为：http://localhost:8080/api/core/getData/userInfo 时
